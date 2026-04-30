18eafae919a1cc08d7acb9fbf4ed8b5517a5a91f
silverlunasen-cmyk <silverlunasen@gmail.com>
2026-04-30 20:26:26 +0100

    Created menu, added default inputs;

---
+++ Menu.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Sysadmin on 30/04/2026.
+//
+
+#include "Menu.h"
+
+#include <iostream>
+
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    cout << "----------------------------------------" << endl;
+    cout << "              BUG BOARD MENU            " << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "1. Initialize Bug Board (load data from file)" << endl;
+    cout << "2. Display all Bugs" << endl;
+    cout << "3. Find a Bug (given an id)" << endl;
+    cout << "4. Tap the Bug Board (causes move all, then fight/eat)" << endl;
+    cout << "5. Display Life History of all Bugs (path taken)" << endl;
+    cout << "6. Display all Cells listing their Bugs" << endl;
+    cout << "7. Run simulation (generates a Tap every second)" << endl;
+    cout << "8. Exit (write Life History of all Bugs to file)" << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "Enter your choice: ";
+    cin >> input;
+
+    switch (input)
+    {
+        case 1:
+        cout << "Initialising Bug board";
+        break;
+        case 2:
+        cout << "Displaying all Bugs (fix the code!)";
+        break;
+        case 3:
+        cout << "Find a Bug (given an id)";
+        break;
+        case 4:
+        cout << "Tap";
+        break;
+        case 5:
+        cout << "History is created by the victors";
+        break;
+        case 6:
+        cout << "cells";
+        break;
+        case 7:
+        cout << "run forest, run";
+        break;
+        case 8:
+        cout << "exit";
+        break;
+        default:
+        cout << "invalid input: please select 1-8";
+        break;
+    }
+
+}
