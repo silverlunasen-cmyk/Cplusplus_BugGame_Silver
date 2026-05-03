1648142eedd8b5a5b918ab95be4edffc5e2b0c10
silverlunasen-cmyk <silverlunasen@gmail.com>
2026-05-03 22:55:39 +0100

    improved file greatly by making it more readable, added the ability to choose a random direction if you are blocked (at the edge of the board), added display method.

--- crawler/Crawler.h
+++ crawler/Crawler.h
@@ -1,28 +1,46 @@
 //
-// Created by D00280022 on 20/04/2026.
+// Created by Sysadmin on 03/05/2026.
 //
 
 #ifndef CPLUSPLUS_BUGGAME_SILVER_CRAWLER_H
 #define CPLUSPLUS_BUGGAME_SILVER_CRAWLER_H
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-#include <list>
-#include <utility>
+#include "../bugs/Bug.h"
 
-#include "bugs/Bug.h"
-
-using namespace std;
 
 class Crawler : public Bug
 {
-    void move();
+    public:
+      using Bug::Bug;
 
+    void move() override
+    {
+        while (blocked)
+        {
+            direction == (rand() % 4) + 1;
+        }
+        if (direction == north)
+            position.second -= 1;
+        else if (direction == south)
+              position.second += 1;
+        else if (direction == east)
+              position.first += 1;
+        else if (direction == west)
+              position.first -= 1;
+    }
+    void display() override
+    {
+        std::string status = (health > 0) ? "Alive" : "Dead";
+        std::string dirName[] = {"", "North", "South", "East", "West"};
 
-
-
+        std::cout << id << " Crawler (" << position.first << "," << position.second << ") "
+                  << health << " " << dirName[direction] << " " << status << std::endl;
+    }
 
 };
-
-
 
 
 #endif //CPLUSPLUS_BUGGAME_SILVER_CRAWLER_H
--- hopper/Hopper.h
+++ hopper/Hopper.h
@@ -1,21 +1,57 @@
 //
-// Created by D00280022 on 20/04/2026.
+// Created by Sysadmin on 03/05/2026.
 //
 
 #ifndef CPLUSPLUS_BUGGAME_SILVER_HOPPER_H
 #define CPLUSPLUS_BUGGAME_SILVER_HOPPER_H
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-#include "bugs/Bug.h"
+#include "../bugs/Bug.h"
 
 
-class Hopper : public Bug
+class Hopper: public Bug
 {
-    private:
-        int hopLength;
-    public:
-    void move;
-};
+private:
+    int hopLength;
 
+public:
+    Hopper(int id, int x, int y, enum direction dir, int health, int hopLength): Bug(id, x, y, dir, health), hopLength(hopLength)
+    {};
+
+    void move() override
+    {
+        while (blocked)
+        {
+            direction == (rand() % 4) + 1;
+
+        }
+        for (int i = 0; i < hopLength; i++)
+        {
+            if (blocked)
+            {
+                break;
+            }
+            if (direction == north) position.second =+ hopLength;
+            else if (direction == south) position.second =- hopLength;
+            else if (direction == east) position.first =+ hopLength;
+            else if (direction == west) position.first =- hopLength;
+
+        }
+        previousPath.push_back(position);
+
+    }
+    void display() override
+    {
+        std::string status = (health > 0) ? "Alive" : "Dead";
+        std::string dirName[] = {"", "North", "South", "East", "West"};
+
+        std::cout << id << " Hopper (" << position.first << "," << position.second << ") "
+                  << dirName[direction] << "" << health << " " << " " << hopLength << " " << status << std::endl;
+    };
+
+};
 
 
 #endif //CPLUSPLUS_BUGGAME_SILVER_HOPPER_H
