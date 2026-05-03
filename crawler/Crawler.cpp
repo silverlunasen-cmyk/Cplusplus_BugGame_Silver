a4211c37edf88d42b0f140ab9fda885676b441e4
silverlunasen-cmyk <silverlunasen@gmail.com>
2026-05-03 22:55:57 +0100

    trivial or unimportant changes to files.

--- crawler/Crawler.cpp
+++ crawler/Crawler.cpp
@@ -1,13 +1,5 @@
 //
-// Created by D00280022 on 20/04/2026.
+// Created by Sysadmin on 03/05/2026.
 //
 
 #include "Crawler.h"
-#include "iostream"
-
-using namespace std;
-
-void Crawler::move()
-{
-    id = 4;
-}
--- hopper/Hopper.cpp
+++ hopper/Hopper.cpp
@@ -1,15 +1,5 @@
 //
-// Created by D00280022 on 20/04/2026.
+// Created by Sysadmin on 03/05/2026.
 //
 
-#include "hopper/Hopper.h"
-#include "iostream";
-
-using namespace std;
-
-void Hopper::move()
-{
-
-
-}
-
+#include "Hopper.h"
