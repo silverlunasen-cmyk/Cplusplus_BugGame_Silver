e9800bbd435ca9d49c0bd80e433d13b99655c32f
silverlunasen-cmyk <silverlunasen@gmail.com>
2026-05-06 21:53:49 +0100

    moved the functions from the h files to the cpp;

--- bugs/Bug.cpp
+++ bugs/Bug.cpp
@@ -1,5 +1,44 @@
-//
-// Created by Sysadmin on 03/05/2026.
-//
-
 #include "Bug.h"
+
+Bug::Bug(int id, int x, int y, enum direction dir, int h)
+    : id(id), position({x, y}), facing(dir), health(h), alive(true)
+{
+    previousPath.push_back(position);
+}
+
+void Bug::displayHistory(std::ostream& out) const
+{
+    bool first = true;
+    for (const auto& pos : previousPath)
+    {
+        if (!first)
+        {
+            out << ", ";
+        }
+        out << "(" << pos.first << "," << pos.second << ")";
+        first = false;
+    }
+
+    if (!isAlive())
+    {
+        out << " (Dead)";
+    }
+    else
+    {
+        out << " (Alive)";
+    }
+}
+
+bool Bug::isAlive() const
+{
+    return health > 0;
+}
+
+bool Bug::isBlocked() const
+{
+    if (facing == north && position.second <= 0) return true;
+    if (facing == south && position.second >= 9) return true;
+    if (facing == east  && position.first >= 9) return true;
+    if (facing == west  && position.first <= 0) return true;
+    return false;
+}
--- bugs/Bug.h
+++ bugs/Bug.h
@@ -1,9 +1,6 @@
-//
-// Created by Sysadmin on 03/05/2026.
-//
-
 #ifndef CPLUSPLUS_BUGGAME_SILVER_BUG_H
 #define CPLUSPLUS_BUGGAME_SILVER_BUG_H
+
 #include <iostream>
 #include <list>
 #include <utility>
@@ -21,68 +18,20 @@ protected:
     std::list<std::pair<int, int>> previousPath;
     bool blocked;
 
-
 public:
-    Bug(int id, int x, int y, enum direction dir, int h)
-            : id(id), position({x, y}), facing(dir), health(h), alive(true)
-    {
-        previousPath.push_back(position);
-    }
+    Bug(int id, int x, int y, enum direction dir, int h);
     virtual ~Bug() {}
 
-    void displayHistory(std::ostream& out = std::cout)
-    {
-        bool first = true;
-        for (const auto& pos : previousPath)
-        {
-            if (!first)
-                {
-                std::cout << ", ";
-            }
-            std::cout << "(" << pos.first << "," << pos.second << ")";
-            first = false;
-        }
-
-        if (!isAlive())
-        {
-            std::cout << " (Dead)";
-        }
-        else
-        {
-            std::cout << " (Alive)";
-        }
-    }
-
-    virtual void display() = 0;
+    void displayHistory(std::ostream& out = std::cout) const;
+    
+    virtual void display() const = 0;
     virtual void move() = 0;
 
-
     int getId() const { return id; }
-
     std::pair<int, int> getPosition() const { return position; }
-    bool isAlive()
-    {
-        if (health <= 0)
-        {
-            return false;
-        }
-        return true;
-    }
-    bool isBlocked()
-    {
-        int x = position.first;
-        int y = position.second;
-        if (facing == north && position.second <= 0) return true;
-        if (facing == south && position.second >= 9) return true;
-        if (facing == east  && position.first >= 9) return true;
-        if (facing == west  && position.first <= 0) return true;
-        return false;
-
-    }
-
-
+    
+    bool isAlive() const;
+    bool isBlocked() const;
 };
 
-
-
-#endif //CPLUSPLUS_BUGGAME_SILVER_BUG_H
+#endif
