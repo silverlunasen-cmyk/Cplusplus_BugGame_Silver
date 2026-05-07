fff3b2bfb30810b412ac820d0d181a8435e8600a
silverlunasen-cmyk <silverlunasen@gmail.com>
2026-05-07 19:16:29 +0100

    fixed a bug where hopper would hop out of bounds;

--- hopper/Hopper.cpp
+++ hopper/Hopper.cpp
@@ -1,4 +1,5 @@
 #include "Hopper.h"
+using namespace std;
 
 Hopper::Hopper(int id, int x, int y, direction dir, int health, int hopLength)
     : Bug(id, x, y, dir, health), hopLength(hopLength) {}
@@ -10,12 +11,37 @@ void Hopper::move()
     while (isBlocked())
     {
         facing = static_cast<direction>((rand() % 4) + 1);
-
     }
-    if (facing == north) position.second += hopLength;
-    else if (facing == south) position.second -= hopLength;
+    if (facing == north) position.second -= hopLength;
+    else if (facing == south) position.second += hopLength;
     else if (facing == east) position.first += hopLength;
     else if (facing == west) position.first -= hopLength;
+
+    if (position.first < 0)
+    {
+        position.first = 0;
+        blocked = true;
+        cout << "you hit a wall! ";
+    }
+        ;
+    if (position.first > 9)
+    {
+        position.first = 9;
+        blocked = true;
+        cout << "you hit a wall! ";
+    }
+    if (position.second < 0)
+    {
+        position.second = 0;
+        blocked = true;
+        cout << "you hit a wall! ";
+    }
+    if (position.second > 9)
+    {
+        position.second = 9;
+        blocked = true;
+        cout << "you hit a wall! ";
+    }
     previousPath.push_back(position);
 
 }
